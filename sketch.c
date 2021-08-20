3a0b1ea10202324591095d4e22582f4c7f4a684a
Edvard Ekrem Sæther <44024237+edvas@users.noreply.github.com>
2021-08-20 12:24:23 +0200

    Update sketch.c

--- sketch.c
+++ sketch.c
@@ -4,7 +4,6 @@ constexpr uint8_t INPUT_BUTTON_START = 8;
 constexpr uint8_t OUTPUT_MOTOR = 10;
 constexpr uint8_t OUTPUT_BUZZER = 6;
 
-bool stared = false;
 unsigned long time_started = 0;
 unsigned long spin_duration = 0; // [ms]
 
@@ -19,15 +18,14 @@ void setup()
   pinMode(OUTPUT_BUZZER, OUTPUT);
   randomSeed(analogRead(0));
   lcd.begin(16, 2);
-  Serial.begin(9600);
-  lcd.print("Start");
+  lcd.print("...");
 }
 
 void loop()
 {
-  Serial.println(analogRead(5));
   if(time_started == 0 && (!digitalRead(INPUT_BUTTON_START) || analogRead(5) < 300))
   {
+#ifdef SOUND
     int i = 1000;
     while(i >= 10)
     {
@@ -36,33 +34,25 @@ void loop()
       delay(i);
       i = 0.83 * i;
     }
+#endif
     
     time_started = millis();
     spin_duration = random(500, 1500); // [ms]
-    //Serial.println(spin_duration);
     analogWrite(OUTPUT_MOTOR, 255); //140
-    //digitalWrite(OUTPUT_MOTOR, HIGH);
 
 #ifdef SOUND
     tone(OUTPUT_BUZZER, 220, 200);
     delay(200);
-    //tone(OUTPUT_BUZZER, 440, 200);
-    //delay(200);
-    //tone(OUTPUT_BUZZER, 220, 200);
-    //delay(200);
-    //tone(OUTPUT_BUZZER, 880, 200);
-    //delay(200);
 #endif
   }
 
   if(time_started != 0 && millis() >= time_started + spin_duration)
   {
     analogWrite(OUTPUT_MOTOR, 0);
-    //digitalWrite(OUTPUT_MOTOR, LOW);
     time_started = 0;
 
-    uint8_t type = random(5);
     lcd.clear();
+    const uint8_t type = random(5);
     switch(type)
     {
       case 0:
@@ -85,13 +75,7 @@ void loop()
     
 #ifdef SOUND
     tone(OUTPUT_BUZZER, 880, 200);
-    //delay(200);
-    //tone(OUTPUT_BUZZER, 440, 200);
-    //delay(200);
-    //tone(OUTPUT_BUZZER, 880, 200);
-    //delay(200);
-    //tone(OUTPUT_BUZZER, 220, 200);
-    //delay(200);
+    delay(200);
 #endif
   }
 }
